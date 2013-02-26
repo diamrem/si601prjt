6ab1834d037e6cfd2fd8613340bd6f9a362617d8
Wenke <wenke@umich.edu>
2013-02-26 00:59:11 -0500

    add time analysis in R

--- wiki.r
+++ wiki.r
@@ -17,3 +17,24 @@ top1
 top1=top1$number.of.edits
 mean(top1)
 sd(top1)
+
+# time --by wenke
+# all dates
+time=read.table("2_output_hour_sorted.txt", header=TRUE,sep="\t")
+plot(time,type='h')
+# weekdays average
+time=read.table("2_output_weekday.txt", header=TRUE,sep="\t")
+plot(time,type='h')
+# weekends average
+time=read.table("2_output_weekend.txt", header=TRUE,sep="\t")
+plot(time,type='h')
+# days in a week
+plot_colors <- c("red","orange","brown","green","blue","purple","black")
+plot(time$Mon, type="l", col=plot_colors[1], ylim=range(time),ylab="Counts",xlab="Hours")
+lines(time$Tue, type="l", col=plot_colors[2])
+lines(time$Wed, type="l", col=plot_colors[3])
+lines(time$Thu, type="l", col=plot_colors[4])
+lines(time$Fri, type="l", col=plot_colors[5])
+lines(time$Sat, type="l", col=plot_colors[6])
+lines(time$Sun, type="l", col=plot_colors[7])
+legend("topleft",names(time[2:8]), cex=0.5,col=plot_colors, bty="n", lwd=1);
